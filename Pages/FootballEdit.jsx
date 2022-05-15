49c755ce5cabb10279175c35044cd2d9e3a825d0
muhammedhamed <96890824+muhammedhamed@users.noreply.github.com>
2022-05-15 02:43:49 +0200

    admin area

--- Pages/FootballEdit.jsx
+++ Pages/FootballEdit.jsx
@@ -1,137 +1,175 @@
-import { TouchableOpacity, StyleSheet, Text, View, TextInput } from 'react-native'
-import React, { useState } from 'react'
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+} from "react-native";
+import React, { useState } from "react";
+import { addFStadium, deleteFStadium } from "../db/Stadium/Football";
+const FootballEdit = ({ navigation }) => {
+  const [id, setid] = useState("");
+  const [name, setname] = useState("");
+  const [pic, setpic] = useState("");
+  const [link, setlink] = useState("");
+  const [date, setdate] = useState("");
+  const [price, setprice] = useState("");
+  const [available, setavailable] = useState("");
+  // const [error, setError] = useState("");
 
-export default function FootBallEdit({ navigation }) {
-    const [id, setId] = useState("");
-    const [name, setName] = useState("");
-    const [pic, setPic] = useState("");
-    const [link, setLink] = useState("");
-    const [date, setDate] = useState("");
-    const [price, setPrice] = useState("");
-    const [available, setAvailable] = useState("");
-    const [error, setError] = useState("");
+  function addfootball() {
+    var football = {
+      name: name,
+      pic: pic,
+      price: price,
+      link: link,
+      date: date,
+      id: id,
+      available: available,
+    };
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>add Stadium</Text>
-            <View>
-                <TextInput
-                    style={styles.textinput}
-                    placeholder="id"
-                    onChangeText={setId}
-                    keyboardType="default"
-                />
+    addFStadium(football);
+    console.log("football", football);
+  }
 
-                <TextInput
-                    style={styles.textinput}
-                    placeholder="name"
-                    onChangeText={setName}
-                    keyboardType="default"
-                />
+  //   function deletefootball() {
+  //     id: id;
+  //     deleteFStadium(id);
+  //   }
 
-                <TextInput
-                    style={styles.textinput}
-                    placeholder="picture link"
-                    onChangeText={setPic}
-                    keyboardType="default"
-                />
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>add Stadium</Text>
+      <View>
+        <TextInput
+          style={styles.textinput}
+          placeholder="id"
+          value={id}
+          onChangeText={(text) => setid(text)}
+          keyboardType="default"
+        />
 
-                <TextInput
-                    style={styles.textinput}
-                    placeholder="location link"
-                    onChangeText={setLink}
-                    keyboardType="default"
-                />
+        <TextInput
+          style={styles.textinput}
+          placeholder="name"
+          value={name}
+          onChangeText={(text) => setname(text)}
+          keyboardType="default"
+        />
 
-                <TextInput
-                    style={styles.textinput}
-                    placeholder="date"
-                    onChangeText={setDate}
-                    keyboardType="default"
-                />
+        <TextInput
+          style={styles.textinput}
+          placeholder="picture link"
+          value={pic}
+          onChangeText={(text) => setpic(text)}
+          keyboardType="default"
+        />
 
-                <TextInput
-                    style={styles.textinput}
-                    placeholder="price"
-                    onChangeText={setPrice}
-                    keyboardType="numeric"
-                />
+        <TextInput
+          style={styles.textinput}
+          placeholder="location link"
+          value={link}
+          onChangeText={(text) => setlink(text)}
+          keyboardType="default"
+        />
 
-                <TextInput
-                    style={styles.textinput}
-                    placeholder="name"
-                    onChangeText={setAvailable}
-                    keyboardType="default"
-                />
-            </View>
-            <View style={styles.format}>
-                <TouchableOpacity style={styles.buttonstyle} onPress={() => { alert('done!') }}>
-                    <Text style={styles.buttontext}>Add</Text>
-                </TouchableOpacity>
-            </View>
-            <View style={styles.line}></View>
+        <TextInput
+          style={styles.textinput}
+          placeholder="date"
+          value={date}
+          onChangeText={(text) => setdate(text)}
+          keyboardType="default"
+        />
 
-            <Text style={styles.title}>delete Stadium</Text>
-            <View>
-                <TextInput
-                    style={styles.textinput}
-                    placeholder="enter id"
-                    onChangeText={setId}
-                    keyboardType="default"
-                />
+        <TextInput
+          style={styles.textinput}
+          placeholder="price"
+          value={price}
+          onChangeText={(text) => setprice(text)}
+          keyboardType="default"
+        />
 
-            </View>
+        <TextInput
+          style={styles.textinput}
+          placeholder="available"
+          value={available}
+          onChangeText={(text) => setavailable(text)}
+          keyboardType="default"
+        />
+      </View>
+      <View style={styles.format}>
+        <TouchableOpacity
+          style={styles.buttonstyle}
+          onPress={() => addfootball()}
+        >
+          <Text style={styles.buttontext}>Add</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.line}></View>
 
-            <View style={styles.format}>
-                <TouchableOpacity style={styles.buttonstyle} onPress={() => { alert('done!') }}>
-                    <Text style={styles.buttontext}>Delete</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    )
-}
+      <Text style={styles.title}>delete Stadium</Text>
+      <View>
+        <TextInput
+          style={styles.textinput}
+          placeholder="enter id"
+          value={id}
+          onChangeText={(text) => setid(text)}
+          keyboardType="default"
+        />
+      </View>
 
+      <View style={styles.format}>
+        <TouchableOpacity
+          style={styles.buttonstyle}
+          onPress={() => deleteFStadium(id)}
+        >
+          <Text style={styles.buttontext}>Delete</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+export default FootballEdit;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#FFF",
-        padding: 20,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        paddingBottom: 10,
-    },
-    buttonstyle: {
-        backgroundColor: '#022b3a',
-        width: '50%',
-        height: 40,
-        marginBottom: 50,
-        borderRadius: 10,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    buttontext: {
-        color: 'rgb(255, 255, 255)',
-        fontWeight: '700',
-        fontSize: 16,
-    },
-    textinput: {
-        height: 40,
-        color: 'rgb(255, 255, 255)',
-        borderBottomColor: "#cae9ff",
-        borderBottomWidth: 4,
-
-    },
-    format: {
-        marginVertical: 20,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    line: {
-        width: '100%',
-        backgroundColor: '#000000',
-        height: 1
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    paddingBottom: 10,
+  },
+  buttonstyle: {
+    backgroundColor: "#022b3a",
+    width: "50%",
+    height: 40,
+    marginBottom: 50,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttontext: {
+    color: "rgb(255, 255, 255)",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  textinput: {
+    height: 40,
+    color: "#000000",
+    borderBottomColor: "#cae9ff",
+    borderBottomWidth: 4,
+  },
+  format: {
+    marginVertical: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  line: {
+    width: "100%",
+    backgroundColor: "#000000",
+    height: 1,
+  },
+});
--- db/Stadium/Football.js
+++ db/Stadium/Football.js
@@ -1,17 +1,42 @@
 import { db } from "../Config";
-import { addDoc, collection, getDocs, getFirestore, setDoc, doc, query, where } from "firebase/firestore";
+import {
+  getDocs,
+  doc,
+  setDoc,
+  addDoc,
+  deleteDoc,
+  collection,
+  query,
+  where,
+  onSnapshot,
+} from "firebase/firestore";
 
-async function addFStadium() {
+async function addFStadium(Football) {
+  try {
+    const docRef = await addDoc(collection(db, "football"), Football);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
 }
 
 async function getFStadium() {
-    const stadiumsCol = collection(db, "football");
-    const stadiumsSnapshot = await getDocs(stadiumsCol);
-    const stadiumList = stadiumsSnapshot.docs.map((doc) => {
-        return { id: doc.id, ...doc.data() };
-    });
-    console.log(stadiumList);
-    return stadiumList;
+  const stadiumsCol = collection(db, "football");
+  const stadiumsSnapshot = await getDocs(stadiumsCol);
+  const stadiumList = stadiumsSnapshot.docs.map((doc) => {
+    return { id: doc.id, ...doc.data() };
+  });
+  console.log(stadiumList);
+  return stadiumList;
 }
 
-export { addFStadium, getFStadium };
+async function deleteFStadium(id) {
+  try {
+    await deleteDoc(doc(db, "football", id));
+    console.log("Document deleted with ID: ", id);
+  } catch (error) {
+    console.error("Error deleting document: ", error);
+  }
+}
+
+export { addFStadium, getFStadium, deleteFStadium };
