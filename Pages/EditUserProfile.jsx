45021889013affdd83b6d4183d2fd15bcba0186a
muhammedhamed <96890824+muhammedhamed@users.noreply.github.com>
2022-05-20 15:47:45 +0200

    edit profile addded

--- Pages/EditUserProfile.jsx
+++ Pages/EditUserProfile.jsx
@@ -1,12 +1,176 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Dimensions,
+  Platform,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { editUser } from "../db/User";
+import { getUserUId } from "../db/Auth";
+import * as ImagePicker from "expo-image-picker";
+import Constants from "expo-constants";
 
-export default function EditUserProfile() {
+import userimage from "../assets/2511582.jpg";
+import { updateProfile } from "firebase/auth";
+export default function EditUserProfile({ navigation }) {
+  const [fullname, setfullname] = useState("");
+  const [phone, setphone] = useState("");
+  const [email, setemail] = useState("");
+  const [idu, setIdu] = useState("");
+  const [piclink, setpiclink] = useState("");
+
+  var user = {
+    fullname: "",
+    email: "",
+    phone: "",
+    piclink: "",
+  };
+
+  getUserUId().then((e) => {
+    setIdu(e);
+  });
+
+  const updateuserinfo = () => {
+    editUser(idu, fullname, phone, email, piclink).then(
+      navigation.navigate("Profile")
+    );
+  };
+
+  useEffect(async () => {
+    if (Platform.OS !== "web") {
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== "granted") {
+        alert("permision denied");
+      }
+    }
+  }, []);
+
+  const PickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log("result", result);
+    if (!result.cancelled) {
+      setpiclink(result.uri);
+    }
+  };
   return (
-    <View>
-      <Text>EditUserProfile</Text>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.addView}>
+        <Text style={styles.title}>update user info</Text>
 
-const styles = StyleSheet.create({})
+        <View style={styles.format}>
+          <TouchableOpacity onPress={PickImage}>
+            <Image source={{ uri: piclink }} style={styles.userimage} />
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <TextInput
+            style={styles.textinput}
+            placeholder="name"
+            value={fullname}
+            onChangeText={(text) => setfullname(text)}
+            keyboardType="default"
+          />
+
+          <TextInput
+            style={styles.textinput}
+            placeholder="phone"
+            value={phone}
+            onChangeText={(text) => setphone(text)}
+            keyboardType="default"
+          />
+
+          <TextInput
+            style={styles.textinput}
+            placeholder="email"
+            value={email}
+            onChangeText={(text) => setemail(text)}
+            keyboardType="default"
+          />
+        </View>
+        <View style={styles.format}>
+          <TouchableOpacity style={styles.buttonstyle} onPress={updateuserinfo}>
+            <Text style={styles.buttontext}>Update</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+const cardwidth = Math.round(Dimensions.get("window").width);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f2e9e4",
+    alignItems: "center",
+  },
+  addView: {
+    backgroundColor: "#22223b",
+    borderRadius: 20,
+    width: cardwidth - 100,
+    height: 500,
+    paddingHorizontal: 25,
+    marginVertical: 15,
+    justifyContent: "center",
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    paddingBottom: 10,
+    color: "#f2e9e4",
+  },
+  buttonstyle: {
+    backgroundColor: "#f2e9e4",
+    width: "50%",
+    height: 40,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttontext: {
+    color: "#22223b",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  textinput: {
+    height: 40,
+    color: "#f2e9e4",
+    borderBottomColor: "#f2e9e4",
+    borderBottomWidth: 4,
+  },
+  format: {
+    marginVertical: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  line: {
+    width: "100%",
+    backgroundColor: "#022b3a",
+    height: 1,
+  },
+  userimage: {
+    height: 150,
+    width: 150,
+    borderRadius: 75,
+    justifyContent: "center",
+  },
+});
--- db/User.js
+++ db/User.js
@@ -11,67 +11,78 @@ import {
   deleteField,
   arrayUnion,
   arrayRemove,
-  deleteDoc
+  deleteDoc,
 } from "firebase/firestore";
 import { getUserUId } from "./Auth";
-import {app, db} from "./Config";
+import { app, db } from "./Config";
 
 const firestoreDB = getFirestore(app);
 
 async function addUser(user) {
-    try {
-        await setDoc(doc(firestoreDB, "users", user.id),  user );
-        console.log("Document written with ID: ", user.id);
-      } catch (e) {
-        console.error("Error adding document: ", e);
-      }
+  try {
+    await setDoc(doc(firestoreDB, "users", user.id), user);
+    console.log("Document written with ID: ", user.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
 }
 
-async function addUsersToDocuments(collectionName,docId,userCollection){
-    console.log("inside "+collectionName)
-    const docRef = doc(db, collectionName, docId);
-    await updateDoc(docRef, {
-        users:userCollection
-    });
+async function addUsersToDocuments(collectionName, docId, userCollection) {
+  console.log("inside " + collectionName);
+  const docRef = doc(db, collectionName, docId);
+  await updateDoc(docRef, {
+    users: userCollection,
+  });
 }
-
 
 async function getUsers() {
-    const usersCol = collection(firestoreDB, "users");
-    const userSnapshot = await getDocs(usersCol);
-    const userList =  userSnapshot.docs.map((doc) => {
-        return {id: doc.id, ...doc.data()};
-    });
-    console.log(userList);
-    return userList;
+  const usersCol = collection(firestoreDB, "users");
+  const userSnapshot = await getDocs(usersCol);
+  const userList = userSnapshot.docs.map((doc) => {
+    return { id: doc.id, ...doc.data() };
+  });
+  console.log(userList);
+  return userList;
 }
 
- async function getUserById(id) {
-    const usersRef = collection(firestoreDB, "users");
-    const q = query(usersRef, where("id", "==", id));
-    const querySnapshot = await getDocs(q);
-    return querySnapshot.docs.map((doc) => {
-        return {id: doc.id, ...doc.data()};
-    });
+async function getUserById(id) {
+  const usersRef = collection(firestoreDB, "users");
+  const q = query(usersRef, where("id", "==", id));
+  const querySnapshot = await getDocs(q);
+  return querySnapshot.docs.map((doc) => {
+    return { id: doc.id, ...doc.data() };
+  });
 }
 
 //                    F-> for "football"
- async function updateFAvailable(tempDoc) {
-     try {
-         console.log(tempDoc);
-         await setDoc(doc(db, "football", tempDoc.id), tempDoc); //if document with "tempDoc.id" not found add it, else update it
-     } catch (e) {
-         console.error(e);
-     }
-
+async function updateFAvailable(tempDoc) {
+  try {
+    console.log(tempDoc);
+    await setDoc(doc(db, "football", tempDoc.id), tempDoc); //if document with "tempDoc.id" not found add it, else update it
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-
-async function editUser(user) {
-  console.log("edit user", user);
-  await setDoc(doc(db, "users", user.id), user);
+async function editUser(docId, fullname, phone, email, piclink) {
+  try {
+    await updateDoc(doc(db, "users", docId), {
+      fullname: fullname,
+      phone: phone,
+      email: email,
+      piclink: piclink,
+    });
+    console.log("Document updated with ID: ", docId);
+  } catch (error) {
+    console.error("Error updateing document: ", error);
+  }
 }
 
-
-
-export {getUsers, addUser,updateFAvailable,addUsersToDocuments , getUserById };
+export {
+  getUsers,
+  addUser,
+  updateFAvailable,
+  addUsersToDocuments,
+  getUserById,
+  editUser,
+};
