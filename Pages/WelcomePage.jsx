2e6c9d75e393bbd87ba5122c30863e949aa58c5a
muhammedhamed <96890824+muhammedhamed@users.noreply.github.com>
2022-05-22 04:29:24 +0200

    logo at welcome page

--- Pages/WelcomePage.jsx
+++ Pages/WelcomePage.jsx
@@ -1,11 +1,12 @@
-import { TouchableOpacity, StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity, StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
+import Logo from "../../assets/2511582.jpg";
 
 const WelcomePage = ({ navigation }) => {
   return (
     <View style={styles.container}>
+      <Image source={Logo} style={styles.imagecover} />
       <Text style={styles.title}>Welcome to Mal3ab!</Text>
-
       <TouchableOpacity
         style={styles.button}
         onPress={() => {
@@ -56,5 +57,13 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#FFF",
     marginBottom: 30,
+  },
+  imagecover: {
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+    backgroundColor: "#22223b",
   },
 });
